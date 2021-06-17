0784d4baef0316f1d3207aac31071b9adabda92d
Özge Akdaş <54955167+ozgeaktas@users.noreply.github.com>
2021-06-17 17:27:58 +0300

    Update baglantı.php

--- baglantı.php
+++ baglantı.php
@@ -1,7 +1,8 @@
 <?php
 
+
 try{
-    $db=new PDO("mysql:host=localhost; dbname=hastane-otomasyon; charest=utf8 ", 'root','');
+    $db=new PDO("mysql:host=sql107.epizy.com; port=3306; dbname=epiz_28902678_hastaneotomasyon; charest=utf8 ", 'epiz_28902678','abGpxkWNwg');
     
 }catch(Exception $e){
     echo $e->getMessage();
